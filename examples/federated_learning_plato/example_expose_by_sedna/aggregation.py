9adb15d1f776ec0db6acbd50d76560319976f98c
Jie Pu <pujie2@huawei.com>
2021-05-14 16:01:06 +0800

    api expose: plato vs sedna

---
+++ examples/federated_learning_plato/example_expose_by_sedna/aggregation.py
@@ -0,0 +1,21 @@
+import os
+
+os.environ['config_file'] = 'examples/demo/server.yml'
+
+from sedna.servers import fedavg
+from .network import model, Trainer
+
+
+class Myserver(fedavg.Server):
+    def __init__(self, model=None, trainer=None):
+        super().__init__(model, trainer)
+
+
+def main():
+    trainer = Trainer(model=model)
+    server = Myserver(model=model, trainer=trainer)
+    server.run()
+
+
+if __name__ == "__main__":
+    main()
